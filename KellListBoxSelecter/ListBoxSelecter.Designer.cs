66917c741983b172501764ae0b4fbb9a18477444
kell888 <421979530@qq.com>
2016-06-02 20:37:08 +0800

    2016-06-02

---
+++ KellListBoxSelecter/ListBoxSelecter.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace KellListBoxSelecter
+{
+    partial class ListBoxSelecter
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(150, 198);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(194, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox2.Size = new System.Drawing.Size(150, 198);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox2_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(3, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = ">>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(3, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(3, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(38, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(3, 71);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(38, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "<";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Location = new System.Drawing.Point(3, 141);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(38, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "排序";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(150, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(44, 198);
+            this.panel1.TabIndex = 7;
+            this.panel1.Resize += new System.EventHandler(this.panel1_Resize);
+            // 
+            // button6
+            // 
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Location = new System.Drawing.Point(3, 168);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(38, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "数据";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // ListBoxSelecter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listBox2);
+            this.ForeColor = System.Drawing.Color.Blue;
+            this.MinimumSize = new System.Drawing.Size(244, 198);
+            this.Name = "ListBoxSelecter";
+            this.Size = new System.Drawing.Size(344, 198);
+            this.Resize += new System.EventHandler(this.ListBoxSelecter_Resize);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button6;
+    }
+}
---
+++ KellListBoxSelecter/ListBoxSelecter.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KellListBoxSelecter
+{
+    [DefaultEvent("SelectedIndexChanged")]
+    public partial class ListBoxSelecter : UserControl
+    {
+        public ListBoxSelecter()
+        {
+            InitializeComponent();
+        }
+
+        List<object> allItems;
+        [Browsable(true)]
+        [Category("属性已更改")]
+        [Description("选择项已改变")]
+        public event EventHandler SelectedIndexChanged;
+
+        [Browsable(true)]
+        [Category("数据")]
+        [Description("供选定项的集合")]
+        public List<object> AllItems
+        {
+            get
+            {
+                return allItems;
+            }
+            set
+            {
+                allItems = value;
+                listBox1.Items.Clear();
+                if (allItems != null)
+                    listBox1.Items.AddRange(allItems.ToArray());
+            }
+        }
+
+        public void RemoveSourceItem(int index)
+        {
+            if (allItems != null && allItems.Count > 0 && index > -1 && index < allItems.Count)
+                allItems.RemoveAt(index);
+            if (listBox1.Items.Count > 0 && index > -1 && index < listBox1.Items.Count)
+                listBox1.Items.RemoveAt(index);
+        }
+
+        public void UpdateSourceItem(int index, object newObj)
+        {
+            if (allItems != null && allItems.Count > 0 && index > -1 && index < allItems.Count)
+                allItems[index] = newObj;
+            if (listBox1.Items.Count > 0 && index > -1 && index < listBox1.Items.Count)
+                listBox1.Items[index] = newObj;
+        }
+
+        [Browsable(false)]
+        public int SelectedSourceIndex
+        {
+            get
+            {
+                return listBox1.SelectedIndex;
+            }
+        }
+
+        [Browsable(false)]
+        public List<object> SelectedItems
+        {
+            get
+            {
+                List<object> items = new List<object>();
+                foreach (object o in listBox2.Items)
+                {
+                    items.Add(o);
+                }
+                return items;
+            }
+        }
+
+        public void SelectSome(List<object> someItems)
+        {
+            listBox2.Items.Clear();
+            listBox2.Items.AddRange(someItems.ToArray());
+        }
+
+        public void ReloadSourceData()
+        {
+            listBox1.Items.Clear();
+            if (allItems != null)
+                listBox1.Items.AddRange(allItems.ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            listBox2.Items.AddRange(allItems.ToArray());
+            listBox1.Items.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                object item = listBox2.Items[i];
+                if (!listBox1.Items.Contains(item))
+                    listBox1.Items.Add(item);
+            }
+            listBox2.Items.Clear();
+            /*
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(allItems);
+            listBox2.Items.Clear();*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems != null && listBox1.SelectedItems.Count > 0)
+            {
+                List<object> items = new List<object>();
+                for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                {
+                    object item = listBox1.SelectedItems[i];
+                    if (!listBox2.Items.Contains(item))
+                    {
+                        listBox2.Items.Add(item);
+                        items.Add(item);
+                    }
+                }
+                foreach (object o in items)
+                {
+                    listBox1.Items.Remove(o);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItems != null && listBox2.SelectedItems.Count > 0)
+            {
+                List<object> items = new List<object>();
+                for (int i = 0; i < listBox2.SelectedItems.Count; i++)
+                {
+                    object item = listBox2.SelectedItems[i];
+                    if (!listBox1.Items.Contains(item))
+                    {
+                        listBox1.Items.Add(item);
+                    }
+                    items.Add(item);
+                }
+                foreach (object o in items)
+                {
+                    listBox2.Items.Remove(o);
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Sorted = true;
+            listBox2.Sorted = true;
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                object item = listBox1.SelectedItem;
+                if (!listBox2.Items.Contains(item))
+                {
+                    listBox2.Items.Add(item);
+                    listBox1.Items.Remove(item);
+                }
+            }
+        }
+
+        private void listBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                object item = listBox2.SelectedItem;
+                if (!listBox1.Items.Contains(item))
+                {
+                    listBox1.Items.Add(item);
+                }
+                listBox2.Items.Remove(item);
+            }
+        }
+
+        private void ListBoxSelecter_Resize(object sender, EventArgs e)
+        {
+            listBox1.Width = listBox2.Width = (this.Width - panel1.Width) / 2;
+        }
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            int add = (panel1.Height - 198) / 2;
+            button1.Top = 7 + add;
+            button3.Top = 42 + add;
+            button4.Top = 71 + add;
+            button2.Top = 106 + add;
+            button5.Top = 141 + add;
+            button6.Top = 168 + add;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (SelectedIndexChanged != null)
+                SelectedIndexChanged(this, EventArgs.Empty);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ReloadSourceData();
+        }
+    }
+}
